27ad6fe9f40eca95b5f44ab1baf2a2b34a32763c
Holley Voegtle <hvoegtle@gmail.com>
2021-11-19 15:09:12 -0800

    check check

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 // import files and packages up here
-
+const express = require('express');
+const topSpots = require('./data');
+const morgan = require('morgan');
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+
+app.get('/', function(req, res) {
+    res.status(200).send('Is this this on?');
+});
+
+app.get('/data', function(req, res) {
+    res.json(topSpots);
+})
 
 // finally export the express application
 module.exports = app;
